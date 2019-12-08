f835ee6adae1b2b246d87578c2a1c287b30db26f
Alind <xhyra.alind@gmail.com>
2019-12-08 23:01:13 +0100

    edit setup.py add setup.cfg

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 
 setup(name='newton_method',
-      version='0.1a',
+      version='v0.1-alpha',
       description='Python Newton gradient method and function plotting',
       author='Alind Xhyra',
       author_email='xhyra.alind@gmail.com',
       url='https://github.com/aXhyra/newton_method',
+      download_url='https://github.com/aXhyra/newton_method/archive/v0.1-alpha.tar.gz',
       packages=['newton_method'],
       requires=['matplotlib', 'numpy']
       )

86fc42f546071a55f566dfb6d2cdea2a2e21d771
Alind <xhyra.alind@gmail.com>
2019-12-08 23:08:06 +0100

    fixed error in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='newton_method',
-      version='v0.1-alpha',
+      version='v0.1.1-alpha',
       description='Python Newton gradient method and function plotting',
       author='Alind Xhyra',
       author_email='xhyra.alind@gmail.com',
       url='https://github.com/aXhyra/newton_method',
       download_url='https://github.com/aXhyra/newton_method/archive/v0.1-alpha.tar.gz',
       packages=['newton_method'],
-      requires=['matplotlib', 'numpy']
+      install_requires=['matplotlib', 'numpy']
       )

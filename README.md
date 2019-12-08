abf097f469a5ac8020b5620c7feedc7093426132
Alind <xhyra.alind@gmail.com>
2019-12-08 22:09:49 +0100

    added README.md

---
+++ README.md
@@ -0,0 +1,45 @@
+newton_method
+=============
+
+##Introduction
+This package contains functions to calculate the minimum of a mathematical function written as a lambda using Newton's
+gradient method (https://en.wikipedia.org/wiki/Newton%27s_method_in_optimization).
+
+##Functions
+This package contains the following functions:
+* df_dx(func) returns a lambda function of the partial derivative on x direction
+* df_dy(func) returns a lambda function of the partial derivative on y direction
+* df_dxdx(func) returns a lambda function of the double partial derivative on x direction
+* df_dxdy(func) returns a lambda function of the mixed double partial derivative on x and y directions
+* df_dydy(func) returns a lambda function of the double partial derivative on y direction
+* df_dydx(func) returns a lambda function of the mixed double partial derivative on y and x directions
+* grad(func) returns an array of lambda functions (gradient of the function func)
+* hess(func) returns a matrix of lambda functions (hessian of the function func)
+* newton(func, x0=(0, 0), steps=2, delta=.05) returns the minimum point of the function from the point x0 with steps
+steps or until the delta is under delta
+* plot(func, start=-10, end=10, bins=100, savefig=False, plotfunc=True, plotgrad=True, filename='') plots the function
+func with its gradient if its a 2 variable function, in case it's a 2D function plots the function, its first and second
+derivative
+
+func has to be a lambda function, for example: 
+'''python
+
+    df_dx(lambda x,y: x ** 2 + y ** 2)
+'''
+
+##Installation
+####pip:
+install this package using pip:
+'''bash
+
+    pip install newton_method
+'''
+####Manual install:
+clone this repo and run setup.py
+'''bash
+ 
+    git clone https://github.com/aXhyra/newton_method
+    cd newton_method
+    pip install .
+'''
+
--- newton_method/__init__.py
+++ newton_method/__init__.py
@@ -1,11 +1,16 @@
-from matplotlib import pyplot as plt
 from inspect import signature
 import numpy as np
 import logging
 from mpl_toolkits import mplot3d
 
 
-def dx(func, epsilon=1e-3):
+def df_dx(func, epsilon=1e-3):
+	"""
+
+	Parameters
+	----------
+	func : object
+	"""
 	nargs = len(signature(func).parameters)
 	if nargs == 2:
 		return lambda x, y: ((func(x + epsilon, y) - func(x, y)) / epsilon)
@@ -14,41 +19,41 @@ def dx(func, epsilon=1e-3):
 	logging.error('Cannot work on functions with more than 2 variables')
 
 
-def dy(func, epsilon=1e-3):
+def df_dy(func, epsilon=1e-3):
 	return lambda x, y: ((func(x, y + epsilon) - func(x, y)) / epsilon)
 
 
-def dx_dx(func):
+def df_dxdx(func):
 	nargs = len(signature(func).parameters)
 	if nargs == 2:
-		return lambda x, y: dx(dx(func))(x, y)
+		return lambda x, y: df_dx(df_dx(func))(x, y)
 	if nargs == 1:
-		return lambda x: dx(dx(func))(x)
+		return lambda x: df_dx(df_dx(func))(x)
 	logging.error('Cannot work on functions with more than 2 variables')
 
 
-def dy_dy(func):
-	return lambda x, y: dy(dy(func))(x, y)
+def df_dydy(func):
+	return lambda x, y: df_dy(df_dy(func))(x, y)
 
 
-def dx_dy(func):
-	return lambda x, y: dx(dy(func))(x, y)
+def df_dxdy(func):
+	return lambda x, y: df_dx(df_dy(func))(x, y)
 
 
-def dy_dx(func):
-	return lambda x, y: dy(dx(func))(x, y)
+def df_dydx(func):
+	return lambda x, y: df_dy(df_dx(func))(x, y)
 
 
 def grad(func):
-	return lambda x, y: np.array([dx(func)(x, y), dy(func)(x, y)])
+	return lambda x, y: np.array([df_dx(func)(x, y), df_dy(func)(x, y)])
 
 
 def hess(func):
-	return lambda x, y: np.array([[dx_dx(func)(x, y), dx_dy(func)(x, y)],
-	                              [dy_dx(func)(x, y), dy_dy(func)(x, y)]])
+	return lambda x, y: np.array([[df_dxdx(func)(x, y), df_dxdy(func)(x, y)],
+	                              [df_dydx(func)(x, y), df_dydy(func)(x, y)]])
 
 
-def newton(func, x0, steps=2, delta=.05):
+def newton(func, x0=(0, 0), steps=2, delta=.05):
 	nargs = len(signature(func).parameters)
 	if nargs == 2:
 		res = np.array([x0[0], x0[1]])
@@ -67,8 +72,8 @@ def newton(func, x0, steps=2, delta=.05):
 
 	elif nargs == 1:
 		res = x0
-		df_dx = dx(func)
-		df_dxdx = dx_dx(func)
+		df_dx = df_dx(func)
+		df_dxdx = df_dxdx(func)
 
 		for i in range(steps):
 			prev = res
@@ -81,16 +86,17 @@ def newton(func, x0, steps=2, delta=.05):
 
 
 def plot(func, start=-10, end=10, bins=100, savefig=False, plotfunc=True, plotgrad=True, filename=''):
+	from matplotlib import pyplot as plt
 	nargs = len(signature(func).parameters)
 	x = np.linspace(start, end, bins)
 
 	if nargs == 1:
 		X = np.linspace(start, end, bins)
 		Y = func(X)
-		df_dx = dx(func)(X)
-		df_dxdx = dx_dx(func)(X)
+		df_dx = df_dx(func)(X)
+		df_dxdx = df_dxdx(func)(X)
 
-		fig = plt.figure(dpi=500)
+		fig = plt.figure(figsize=(8, 6))
 		ax = fig.add_subplot(2, 2, 1, title='f(x)')
 		ax.set_xlabel('x')
 		ax.set_ylabel('y')
@@ -111,9 +117,9 @@ def plot(func, start=-10, end=10, bins=100, savefig=False, plotfunc=True, plotgr
 		gd = gradient(X, Y)
 		Z = func(X, Y)
 
-		fig = plt.figure(dpi=500)
+		fig = plt.figure(figsize=(8, 6))
 		if plotfunc and plotgrad:
-			ax = fig.add_subplot(1, 2, 1, projection='3d', title='f(x,y) = 2x^2 + xy + 2(y-3)^2', adjustable='box',
+			ax = fig.add_subplot(1, 2, 1, projection='3d', title='f(x,y)', adjustable='box',
 			                     aspect=1)
 			ax.contour3D(X, Y, Z, 50, cmap='binary')
 			ax.set_xlabel('x')
@@ -122,7 +128,7 @@ def plot(func, start=-10, end=10, bins=100, savefig=False, plotfunc=True, plotgr
 			b = fig.add_subplot(1, 2, 2, title='gradient', adjustable='box', aspect=1)
 			b.quiver(X, Y, gd[0], gd[1])
 		elif plotfunc:
-			ax = fig.add_subplot(1, 1, 1, projection='3d', title='f(x,y) = 2x^2 + xy + 2(y-3)^2', adjustable='box',
+			ax = fig.add_subplot(1, 1, 1, projection='3d', title='f(x,y)', adjustable='box',
 			                     aspect=1)
 			ax.contour3D(X, Y, Z, 50, cmap='binary')
 			ax.set_xlabel('x')
